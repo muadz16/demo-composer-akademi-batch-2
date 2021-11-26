d90fb21f405a124ad7bbd5078f09f2b2c90600c8
root <root@mgt-dev>
2021-11-26 09:58:17 +0000

    first commit

---
+++ src/HaloDunia.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace muadz16;
+
+class HaloDUnia
+{
+	public function getMessage()
+	{
+		return "Halo Dunia Dari Akademi Batch 2";
+	}
+}
---
+++ test/testHaloDunia.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use muadz16\HaloDunia;
+
+$haloDunia = new HaloDunia();
+
+echo $haloDunia->getMessage();
